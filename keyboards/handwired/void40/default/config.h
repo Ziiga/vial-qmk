466ab5393bf2167e4a76af8c34c040536450ac77
Žiga <ziga.zunec@gmail.com>
2023-07-02 23:46:24 +0200

    1.0

--- keyboards/handwired/void40/default/config.h
+++ keyboards/handwired/void40/default/config.h
@@ -24,7 +24,8 @@
 
 /* Keyboard Matrix Assignments */
  #define MATRIX_ROW_PINS { D3, D2, D1, D0 }
- #define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5, F4, F5, F6, F7, B1, B3 }
+ //#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5, F4, F5, F6, F7, B1, B3 }
+ #define MATRIX_COL_PINS { B3, B1, F7, F6, F5, F4, B5, B4, E6, D7, C6, D4 }
 
 /* Rotary Encoder Support */
 #define ENCODERS_PAD_A { B6 }
